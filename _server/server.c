910a694c07774328e9b441cd67a20bd2f99938a7
Rain Maker <hadrien.allegaert@student.uclouvain.be>
2022-10-11 14:43:22 +0200

    server parsing patch

--- _server/server.c
+++ _server/server.c
@@ -9,30 +9,31 @@ int main(int argc, char **argv){
     printf("Server running...\n");
 
 
-    int thread_n;
-    int file_size;
-    int port;
+    int thread_n = 0;
+    int file_size = 0;
+    int port = 0;
 
-    int opt;
+    int index = 1;
     char* err;
-    while((opt = getopt(argc, argv, "j:s:p")) != -1){
-        switch(opt){
+    while(index < argc){
+        char cmd = argv[index++][1];
+        switch(cmd){
             case 'j':
-                thread_n = strtol(argv[optind-1], &err, 10);
+                thread_n = strtol(argv[index++], &err, 10);
                 if(*err != '\0' || thread_n <= 0){
                     printf("Argument Error: Thread_number (-j) should be a positive number.\n");
                     return EXIT_FAILURE;
                 }
                 break;
             case 's':
-                file_size = strtol(argv[optind-1], &err, 10);
+                file_size = strtol(argv[index++], &err, 10);
                 if(*err != '\0' || file_size <= 0){
                     printf("Argument Error: File Size (-s) should be a positive number.\n");
                     return EXIT_FAILURE;
                 }
                 break;
             case 'p':
-                port = strtol(argv[optind], &err, 10);
+                port = strtol(argv[index++], &err, 10);
                 if(*err != '\0' || port <= 0){
                     printf("Argument Error: Port (-p) should be a positive number.\n");
                     return EXIT_FAILURE;
@@ -46,4 +47,3 @@ int main(int argc, char **argv){
     /// Code the real server here
     return EXIT_SUCCESS;
 }
-
