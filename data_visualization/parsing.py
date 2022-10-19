6a6e5d5dcd92317ba161fef11659b2ac1687c821
Rain Maker <hadrien.allegaert@student.uclouvain.be>
2022-10-19 21:02:59 +0200

    add errors and no file received tracking

--- data_visualization/parsing.py
+++ data_visualization/parsing.py
@@ -1,15 +1,23 @@
 def parse(filename):
-    infos = {"thread_number": 0, "request_rate": 0, "request_time": 0, "run_times": []}
+    infos = {"thread_number": 0, "request_rate": 0, "request_time": 0, "run_times": [], "errors": 0, "no_file": 0}
     data = []
     with open(filename, 'r') as file:
         for line in file:
-            if "Elapsed time" in line:
-                data.append(int(line[39:-4])/1000000)  # store and convert in seconds
-            elif "Total threads" in line:
+            # basics infos
+            if "Total threads" in line:
                 infos["thread_number"] = int(line[22:-1])
             elif "Request rate" in line:
                 infos["request_rate"] = int(line[14:-1])
             elif "Request time" in line:
                 infos["request_time"] = int(line[14:-1])
+            # All ok run time store and convert in seconds
+            elif "Elapsed time" in line:
+                data.append(int(line[39:-4])/1000000)
+            # file size was equal to 0
+            elif "No file received" in line:
+                infos["no_file"] += 1
+            # when server sent error code
+            elif "error code" in line:
+                infos["errors"] += 1
     infos["run_times"] = data
     return infos
